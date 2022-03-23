f869c5db5cd0a73f21a35f86bc99660d7ce7820d
pado0 <bgshhd95@gmail.com>
2022-03-24 00:52:34 +0900

    0324 youtube lecture

--- src/main/java/com/pado/SpringBootPractice/SpringBootPracticeApplication.java
+++ src/main/java/com/pado/SpringBootPractice/SpringBootPracticeApplication.java
@@ -3,6 +3,7 @@ package com.pado.SpringBootPractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 스프링은 톰캣 웹서버를 내장하고 있다. SpringBootApplication 어노케이션 실행시 톰캣을 띄우며 서버를 실행한다.
 @SpringBootApplication
 public class SpringBootPracticeApplication {
 
